7865c2ce24900c7ee3a3740558735ef39a14fbe2
Resendii <gabriel@sensio.com.br>
2023-06-24 20:30:23 -0300

    feat(Private Routes): Private Routes
    set tab icons

--- src/Routes/PrivateRoutes.js
+++ src/Routes/PrivateRoutes.js
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Rides from "../screens/private/Rides/Rides";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
 import RideStack from "./Stacks/RideStack";
 import Search from "../screens/private/Search";
 import ChatStack from "./Stacks/ChatStack";
@@ -10,10 +11,50 @@ const Tab = createBottomTabNavigator();
 export default () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name='Search' component={Search} />
-      <Tab.Screen name='Rides' component={RideStack} />
-      <Tab.Screen name='Chats' component={ChatStack} />
-      <Tab.Screen name='Profile' component={Profile} />
+      <Tab.Screen
+        name='Search'
+        component={Search}
+        options={{
+          headerShown: false,
+          tabBarLabel: "Buscar",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name='home' color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Rides'
+        component={RideStack}
+        options={{
+          headerShown: false,
+          tabBarLabel: "Caronas",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name='car' color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Chats'
+        component={ChatStack}
+        options={{
+          headerShown: false,
+          tabBarLabel: "Chats",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name='chat' color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Profile'
+        component={Profile}
+        options={{
+          headerShown: false,
+          tabBarLabel: "Perfil",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name='account' color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
