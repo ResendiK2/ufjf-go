03df819f0a8ac131a2b7ab46e76c2b7acab64c8e
Resendii <gabriel@sensio.com.br>
2023-06-24 20:27:24 -0300

    fix(Ride): Ride screen
    fix screen

--- src/screens/private/Rides/Ride/index.js
+++ src/screens/private/Rides/Ride/index.js
@@ -4,7 +4,7 @@ import { VStack, Text } from "native-base";
 export default () => {
   return (
     <VStack flex={1} justifyContent='center' alignItems='center'>
-      <Text>Profile</Text>
+      <Text>Ride</Text>
     </VStack>
   );
 };
