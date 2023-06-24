36d18fe218a4084970e82e503bd9c5c9a012d516
Resendii <gabriel@sensio.com.br>
2023-06-24 20:09:36 -0300

    feat(Screens): set screen files

---
+++ src/screens/public/Preload/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { VStack, Text } from "native-base";
+
+export default () => {
+  return (
+    <VStack flex={1} justifyContent='center' alignItems='center'>
+      <Text>Preload</Text>
+    </VStack>
+  );
+};
