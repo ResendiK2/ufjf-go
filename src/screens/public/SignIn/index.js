3a29eafcba95ebed5c4138ccb90e978a360f4444
Resendii <gabriel@sensio.com.br>
2023-06-27 22:38:08 -0300

    refactor(SignIn/Up): refactor/style/standard

--- src/screens/public/SignIn/index.js
+++ src/screens/public/SignIn/index.js
@@ -1,67 +1,73 @@
-import React, { useState } from "react";
-import { VStack, Text, Input, Button, HStack, Link, Heading, FormControl, Center, Box, Image } from 'native-base';
+import React, { useContext, useState } from "react";
+import { VStack, Text, Input, Button, HStack, FormControl } from "native-base";
 
-import LogoImg from '../../../../assets/App.jpeg'
+import Ionicons from "react-native-vector-icons/FontAwesome5";
+import { Context } from "../../../Providers/context";
 
+export default ({ navigation }) => {
+  const { setIsLogged } = useContext(Context);
 
-export default () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = () => {
-    console.log('Email:', email);
-    console.log('Password:', password);
-  };
-  return (
-    <VStack flex={1} justifyContent='center' alignItems='center'>
-      <Center w="100%">
-        <Image shadow={2}
-          source={LogoImg}
-          alt="Alternate Text"
-          resizeMode="contain" />
-        <Box safeArea p="2" py="8" w="90%" maxW="290">
-          <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
-            color: "warmGray.50"
-          }}>
-            Bem vindo
-          </Heading>
+    console.log("Email:", email);
+    console.log("Password:", password);
 
-          <VStack space={3} mt="5">
-            <FormControl>
-              <FormControl.Label>Login</FormControl.Label>
-              <Input onChangeText={(text) => setEmail(text)} />
-            </FormControl>
-            <FormControl>
-              <FormControl.Label>Senha</FormControl.Label>
-              <Input type="password" onChangeText={(text) => setPassword(text)} />
-              <Link _text={{
-                fontSize: "xs",
-                fontWeight: "500",
-                color: "indigo.500"
-              }} alignSelf="flex-end" mt="1">
-                Esqueci minha senha
-              </Link>
-            </FormControl>
-            <Button mt="2" colorScheme="indigo" onPress={handleLogin}>
-              Entrar
-            </Button>
-            <HStack mt="6" justifyContent="center">
-              <Text fontSize="sm" color="coolGray.600" _dark={{
-                color: "warmGray.200"
-              }}>
-                Novo por aqui?{" "}
-              </Text>
-              <Link _text={{
-                color: "indigo.500",
-                fontWeight: "medium",
-                fontSize: "sm"
-              }} href="#">
-                Registrar
-              </Link>
-            </HStack>
-          </VStack>
-        </Box>
-      </Center>
+    setIsLogged(true);
+  };
+
+  return (
+    <VStack
+      flex='1'
+      space='5'
+      paddingX='10'
+      paddingY='10'
+      bgColor='white'
+      justifyContent='center'
+    >
+      <HStack justifyContent='space-evenly' alignItems='center'>
+        <Ionicons name='car' size={50} color='#8b5cf6' />
+        <Text fontSize='5xl' fontWeight='bold' color='primary.500'>
+          UFJFGo
+        </Text>
+      </HStack>
+
+      <FormControl>
+        <VStack space='5'>
+          <Input
+            type='email'
+            placeholder='Email'
+            onChangeText={(text) => setEmail(text)}
+          />
+
+          <Input
+            type='password'
+            placeholder='Senha'
+            onChangeText={(text) => setPassword(text)}
+          />
+        </VStack>
+      </FormControl>
+
+      <VStack space='3'>
+        <Button w='100%' onPress={() => handleLogin()}>
+          Entrar
+        </Button>
+        <Button
+          w='100%'
+          variant='outline'
+          onPress={() => navigation.navigate("SignUp")}
+        >
+          Cadastrar
+        </Button>
+        <Button
+          w='100%'
+          variant='link'
+          onPress={() => navigation.navigate("ForgetPassword")}
+        >
+          Esqueci minha senha
+        </Button>
+      </VStack>
     </VStack>
   );
 };
--- src/screens/public/SignUp/index.js
+++ src/screens/public/SignUp/index.js
@@ -1,93 +1,156 @@
 import React, { useState } from "react";
-import { VStack, Text, Input, Button, HStack, Link, Heading, FormControl, Center, Box, Image, Select, CheckIcon, Stack, Checkbox, Alert, ChevronLeftIcon, ScrollView } from 'native-base';
+import {
+  VStack,
+  Text,
+  Input,
+  Button,
+  HStack,
+  FormControl,
+  Center,
+  Select,
+  ScrollView,
+  Switch,
+  Icon,
+} from "native-base";
 
-import UploadSvg from "../../../../assets/upload.svg"
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 export default () => {
-  const [error, setError] = useState(false)
+  const [error, setError] = useState(false);
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [isDriver, setIsDriver] = useState(false);
-  const [userData, setUserData] = useState({})
-
+  const [userData, setUserData] = useState({});
 
   const verifyPassword = () => {
     if (password != confirmPassword) {
-      setError(true)
+      setError(true);
     } else {
-      setUserData({ ...userData, confirmPassword })
-      setError(false)
+      setUserData({ ...userData, confirmPassword });
+      setError(false);
     }
-  }
+  };
 
   return (
-    <ScrollView h="80">
-      <VStack flex={1} justifyContent='center' alignItems='center'>
-        <Center w="100%">
-          <Box safeArea p="2" py="8" w="90%" maxW="290"><HStack space={6} mt={6}>
-            <Button variant="unstyled">
-              <ChevronLeftIcon />
-            </Button>
-            <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
-              color: "warmGray.50"
-            }}>
-              Registro
-            </Heading>
-          </HStack>
+    <ScrollView bgColor='white'>
+      <VStack space='5' paddingX='10' paddingY='10'>
+        <FormControl>
+          <VStack space='5'>
+            <Input
+              placeholder='Nome'
+              type='text'
+              onChangeText={(value) =>
+                setUserData({ ...userData, name: value })
+              }
+            />
 
-            <VStack space={3} mt="5">
-              <FormControl>
-                <Input mt={6} placeholder="Nome" type="text" onChangeText={(value) => setUserData({ ...userData, name: value })} />
-                <Input mt={6} placeholder="Sobrenome" type="text" onChangeText={(value) => setUserData({ ...userData, lastName: value })} />
-                <Input mt={6} placeholder="Email" type="text" onChangeText={(value) => setUserData({ ...userData, email: value })} />
-                <Input mt={6} placeholder="Idade" type="text" onChangeText={(value) => setUserData({ ...userData, age: value })} />
-                <Input mt={6} placeholder="Senha" type="password" onChangeText={(value) => setPassword(value)} />
-                <Input mt={6} placeholder="Confirme sua senha"
-                  type="password"
-                  onChangeText={(value) => setConfirmPassword(value)}
-                  onBlur={() => verifyPassword()}
-                />
-                {error ?
-                  <>
-                    <FormControl.HelperText _text={{
-                      fontSize: 'xs'
-                    }}>
-                      Senhas são diferentes
-                    </FormControl.HelperText>
-                  </> : null
+            <Input
+              placeholder='Sobrenome'
+              type='text'
+              onChangeText={(value) =>
+                setUserData({ ...userData, lastName: value })
+              }
+            />
+
+            <Input
+              placeholder='Email'
+              type='text'
+              onChangeText={(value) =>
+                setUserData({ ...userData, email: value })
+              }
+            />
+
+            <Input
+              placeholder='Idade'
+              type='text'
+              onChangeText={(value) => setUserData({ ...userData, age: value })}
+            />
+
+            <Input
+              placeholder='Senha'
+              type='password'
+              onChangeText={(value) => setPassword(value)}
+            />
+
+            <Center justifyContent='center' alignItems='center'>
+              <Input
+                type='password'
+                placeholder='Confirme sua senha'
+                onChangeText={(value) => setConfirmPassword(value)}
+                onBlur={() => verifyPassword()}
+              />
+              {error ? (
+                <Text color='red.500' fontSize='xs'>
+                  As senhas não coincidem!
+                </Text>
+              ) : null}
+            </Center>
+
+            <Select
+              selectedValue={userData.gender}
+              accessibilityLabel='Choose gender'
+              placeholder='Como você se identifica'
+              onValueChange={(value) =>
+                setUserData({ ...userData, gender: value })
+              }
+            >
+              <Select.Item label='Masculino' value='masculino' />
+              <Select.Item label='Feminino' value='feminino' />
+              <Select.Item label='Outro' value='outro' />
+            </Select>
+
+            <HStack alignItems='center'>
+              <Switch
+                mr={5}
+                size='lg'
+                isChecked={isDriver}
+                onToggle={() => setIsDriver(!isDriver)}
+              />
+              <Text color='primary.500' fontSize='md'>
+                Também sou motorista
+              </Text>
+            </HStack>
+
+            {isDriver ? (
+              <Button
+                size='lg'
+                leftIcon={
+                  <Icon
+                    as={Ionicons}
+                    name='cloud-upload-outline'
+                    size='5xl'
+                    mr='3'
+                  />
                 }
-                <Select selectedValue={userData.gender} accessibilityLabel="Choose gender" placeholder="Como você se identifica" _selectedItem={{
-                  bg: "teal.600",
-                  endIcon: <CheckIcon />
-                }} mt={6} onValueChange={(value) => setUserData({ ...userData, gender: value })} >
-                  <Select.Item label="Masculino" value="masculino" />
-                  <Select.Item label="Feminino" value="feminino" />
-                  <Select.Item label="Outro" value="outro" />
-                </Select>
-                <HStack space={6} mt={6}>
-                  <Text>Você é motorista?</Text>
-                  <Checkbox onChange={() => setIsDriver(!isDriver)} accessibilityLabel="This is a dummy checkbox" />
-                </HStack>
-                {isDriver ? <>
-                  <FormControl.Label mt={6}>Carteira de Motorista</FormControl.Label>
-                  <Center mt={6}>
-                    <UploadSvg height={50} width={50} />
-                  </Center>
-                </> :
-                  <></>
-                }
-                <FormControl.Label mt={6}>Comprovante de Matricula</FormControl.Label>
-                <Center mt={6}>
-                  <UploadSvg height={50} width={50} />
-                </Center>
-              </FormControl>
-              <Button mt="2" colorScheme="indigo" onPress={() => verifyPassword()} >
-                Cria conta
+                variant='ghost'
+              >
+                Carteira de Motorista
               </Button>
-            </VStack>
-          </Box >
-        </Center >
-      </VStack >
+            ) : null}
+
+            <Button
+              size='lg'
+              leftIcon={
+                <Icon
+                  as={Ionicons}
+                  name='cloud-upload-outline'
+                  size='5xl'
+                  mr='3'
+                />
+              }
+              variant='ghost'
+            >
+              Comprovante de Matrícula
+            </Button>
+          </VStack>
+        </FormControl>
+
+        <Center mt='5'>
+          <Button w='100%' onPress={verifyPassword}>
+            Cria conta
+          </Button>
+        </Center>
+      </VStack>
     </ScrollView>
   );
 };
