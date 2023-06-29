315779cc2198b54ccd198a9c17ab23a56baa8559
Resendii <gabriel@sensio.com.br>
2023-06-28 22:09:15 -0300

    feat(RideStack): Add RideForm screen

--- src/Routes/Stacks/RideStack/index.js
+++ src/Routes/Stacks/RideStack/index.js
@@ -1,18 +1,35 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Rides from "../../../screens/private/Rides/Rides";
-import Ride from "../../../screens/private/Rides/Ride";
+import Rides from "../../../Screens/private/Rides/Rides";
+import Ride from "../../../Screens/private/Rides/Ride";
+import RidesForm from "../../../Screens/private/Rides/RidesForm";
 
 const Stack = createNativeStackNavigator();
 
 export default () => {
   return (
-    <Stack.Navigator
-      initialRouteName='Rides'
-      screenOptions={{ headerShown: false }}
-    >
-      <Stack.Screen name='Rides' component={Rides} />
-      <Stack.Screen name='Ride' component={Ride} />
+    <Stack.Navigator initialRouteName='Rides'>
+      <Stack.Screen
+        name='Rides'
+        component={Rides}
+        options={{
+          headerTitle: "Caronas",
+        }}
+      />
+      <Stack.Screen
+        name='Ride'
+        component={Ride}
+        options={{
+          headerTitle: "Carona",
+        }}
+      />
+      <Stack.Screen
+        name='RidesForm'
+        component={RidesForm}
+        options={{
+          headerTitle: "Formulário de Carona",
+        }}
+      />
     </Stack.Navigator>
   );
 };
